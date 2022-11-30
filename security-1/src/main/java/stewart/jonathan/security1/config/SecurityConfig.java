280b44a0c3308d30de4d41c3abf9ef7f94a6fc6f
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-11-30 12:23:34 +0000

    update

--- security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
+++ security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,75 +16,43 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> {
+//                    auth.anyRequest().authenticated();
+//                })
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .httpBasic(Customizer.withDefaults())
+//                .build();
+//    }
     
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .authorities("read")
                 .build();
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("password")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+//        UserDetails admin = User.withUsername("admin")
+//                .password("password")
+//                .roles("ADMIN")
+//                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/").permitAll();
-            auth.requestMatchers("/user").hasRole("USER");
-            auth.requestMatchers("/admin").hasRole("ADMIN");
+            auth.anyRequest().authenticated();
+//            auth.requestMatchers("/user").hasAuthority("read");
+//            auth.requestMatchers("/admin").hasRole("ADMIN");
         }).formLogin();
         return http.build();
     }
+
 }
-
-
-// DAN VEGA WAY - authorizeRequests deprecated
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager() {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("password")
-//                .roles("ADMIN")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
-
-
-
-//        return http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeRequests(auth -> {
-//                    auth.antMatchers("/").permitAll();
-//                    auth.antMatchers("/user").hasRole("USER");
-//                    auth.antMatchers("/admin").hasRole("ADMIN");
-//                })
-//                .httpBasic(Customizer.withDefaults())
-//                .build();
-
-        // OLD NON-LAMDA WAY TO DO IT
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/user").hasRole("USER")
-//                .antMatchers("/admin").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .build();
 
 
--- security-1/src/main/java/stewart/jonathan/security1/controller/HomeController.java
+++ security-1/src/main/java/stewart/jonathan/security1/controller/HomeController.java
@@ -3,21 +3,15 @@ package stewart.jonathan.security1.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "Hello World";
+    public String home(Principal principal) {
+
+        return "Hello " + principal.getName();
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "Hello user";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "Hello Admin";
-    }
 }
