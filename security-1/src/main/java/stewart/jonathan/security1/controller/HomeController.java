4c87218991a15cb6cd21587e894719ceb3434428
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-11-30 10:02:29 +0000

    initial demo project

---
+++ security-1/src/main/java/stewart/jonathan/security1/controller/HomeController.java
@@ -0,0 +1,23 @@
+package stewart.jonathan.security1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello Admin";
+    }
+}
