04ca6f37c597a48157582dd84eabcf07c04aab66
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-12-01 10:16:42 +0000

    pre-authorize

--- security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
+++ security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
@@ -4,6 +4,8 @@ package stewart.jonathan.security1.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,10 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static stewart.jonathan.security1.config.UserPermission.*;
 import static stewart.jonathan.security1.config.UserRole.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final PasswordEncoder passwordEncoder;
@@ -30,23 +34,37 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername("user")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                //.roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails admin = User.withUsername("admin")
                 .password(passwordEncoder.encode("password"))
-                .roles(ADMIN.name())
+                //.roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+
+        UserDetails trainee = User.withUsername("trainee")
+                .password(passwordEncoder.encode("password"))
+                //.roles(ADMINTRAINEE.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+        return new InMemoryUserDetailsManager(user, admin, trainee);
     }
 
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/index.html", "/").permitAll()
                         .requestMatchers("/api/v1/students/*").hasRole("ADMIN")
+  // removed due to @PreAuthorize in controller
+//                        .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                        .requestMatchers(HttpMethod.GET, "/management/api/*").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                         .anyRequest().authenticated()
                 ).formLogin().and().httpBasic().and().logout();
 
--- security-1/src/main/java/stewart/jonathan/security1/config/UserRole.java
+++ security-1/src/main/java/stewart/jonathan/security1/config/UserRole.java
@@ -1,20 +1,35 @@
 package stewart.jonathan.security1.config;
 
-
-
 import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static stewart.jonathan.security1.config.UserPermission.*;
 
 public enum UserRole {
     STUDENT(Sets.newHashSet()),
-    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
+    ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
 
     private final Set<UserPermission> permissions;
 
     UserRole(Set<UserPermission> permissions) {
         this.permissions = permissions;
+    }
+
+    public Set<UserPermission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(p -> new SimpleGrantedAuthority(p.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
     }
 }
