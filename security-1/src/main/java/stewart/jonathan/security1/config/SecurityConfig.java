e27d5de744df8da849808afec4ba9f263f57b34d
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-12-02 12:23:41 +0000

    updating JWT progress

--- security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
+++ security-1/src/main/java/stewart/jonathan/security1/config/SecurityConfig.java
@@ -11,9 +11,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import stewart.jonathan.security1.auth.UserService;
+import stewart.jonathan.security1.jwt.JwtTokenVerifier;
+import stewart.jonathan.security1.jwt.JwtUsernamePasswordAuthenticationFilter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -52,57 +55,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configureSecurity(HttpSecurity http) throws Exception {
+        final AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
         http
                 .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager))
+                .addFilterAfter(new JwtTokenVerifier(), JwtUsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("index", "/", "/css/*", "/js/*").permitAll()
                         .requestMatchers("/api/**").hasRole(STUDENT.name())
                         .anyRequest()
                         .authenticated()
-                )
-                .formLogin()
-                    .loginPage("/login").permitAll()
-                    .defaultSuccessUrl("/courses", true)
-                .passwordParameter("password") // this is default anyway so don't need to put this in
-                .usernameParameter("username") // links to what name you called in in html file
-                .and()
-                .rememberMe().tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                .key("somethingverysecure")
-                .rememberMeParameter("remember-me")
-                .and()
-                .logout().logoutUrl("/logout")
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/login");
+                );
         return http.build();
     }
 
-    //    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withUsername("user")
-//                .password(passwordEncoder.encode("password"))
-//                //.roles(STUDENT.name())
-//                .authorities(STUDENT.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails admin = User.withUsername("admin")
-//                .password(passwordEncoder.encode("password"))
-//                //.roles(ADMIN.name())
-//                .authorities(ADMIN.getGrantedAuthorities())
-//                .build();
-//
-//        UserDetails trainee = User.withUsername("trainee")
-//                .password(passwordEncoder.encode("password"))
-//                //.roles(ADMINTRAINEE.name())
-//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin, trainee);
-//    }
-
 }
 
-// TIMESTAMP 3:09 https://www.youtube.com/watch?v=her_7pa0vrg
+// TIMESTAMP 4:31 https://www.youtube.com/watch?v=her_7pa0vrg
 
 
 
