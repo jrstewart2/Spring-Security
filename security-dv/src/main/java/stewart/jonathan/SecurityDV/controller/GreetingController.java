2e43d5047c1ddcf9e84fb33b69fe9fe30228dfe8
jrstewart2 <jonathan.stewart2@nationwide.co.uk>
2022-12-02 16:13:48 +0000

    updates

---
+++ security-dv/src/main/java/stewart/jonathan/SecurityDV/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package stewart.jonathan.SecurityDV.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Greetings";
+    }
+}
